fa7e871ff4aa8af1259e9d3d5dc6f0f1fc0b0d76
Shail Patel <shailbrijeshkumar.patel@sjsu.edu>
2023-02-25 23:38:32 -0800

    added and removed backend apps

--- private_gym/settings.py
+++ private_gym/settings.py
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'backend',
+    # Django app imports
+    'User',
+    'Gym',
+    'Business',
+    'Daily',
 ]
 
 MIDDLEWARE = [
