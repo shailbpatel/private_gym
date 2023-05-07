606b8d91b2ba11a5e36b6003b46b64d525452ca9
Shail Patel <shailbrijeshkumar.patel@sjsu.edu>
2023-05-06 17:19:08 -0700

    added user role to user_details API

--- User/serializers.py
+++ User/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework.serializers import ModelSerializer
 from User.models import User, Enrolled
 from Business.serializers import ClassSerializer
+from rest_framework import serializers
 
 class UserSerializer(ModelSerializer):
+    role = serializers.SerializerMethodField()
     class Meta:
         model = User
-        fields=('id', 'fname', 'lname', 'email', 'phone')
+        fields=('id', 'fname', 'lname', 'email', 'phone', 'role',)
+
+    def get_role(self, obj):
+        if obj.role == 0:
+            return 'admin'
+        elif obj.role == 1:
+            return 'member'
+        elif obj.role == 2:
+            return 'non-member'
 
 
 class EnrolledSerializer(ModelSerializer):
